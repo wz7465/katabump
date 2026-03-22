33a4337640981abe1951e1561d2cd48dcfcaada0
wz <wz7465@msn.com>
2026-03-22 18:23:46 +0800

    Update retry_if_needed.sh

--- retry_if_needed.sh
+++ retry_if_needed.sh
@@ -6,13 +6,9 @@ RETRY=".need_retry"
 if [ -f "$RETRY" ]; then
     echo "Retrying task..."
 
-    RESULT=$(xvfb-run --auto-servernum --server-args="-screen 0 1280x720x24" node action_renew.js)
-
-    echo "$RESULT"
-
-    if echo "$RESULT" | tail -n 20 | grep -q '"success":true'; then
+    if xvfb-run --auto-servernum --server-args="-screen 0 1280x720x24" node action_renew.js; then
         echo "Retry succeeded."
-        date +%s > "$STATE"
+        date +%s > $STATE
         rm -f "$RETRY"
     else
         echo "Retry failed. Will try again in 4 hours."
